f2d2a6fa770a4f5ffae49c70215ca310697494a5
“Virginia <vbernasconi@altimetrik.com>
2022-07-06 21:03:45 -0300

    first lesson

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,10 @@
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+ */
+
+const country = "Argentina";
+const continent = "America";
+const population = 45.38;
+
+console.log(country, continent, population);
