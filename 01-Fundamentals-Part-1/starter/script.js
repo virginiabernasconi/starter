f2d161663776f661a8eaf717b43be0c45d349b9b
“Virginia <vbernasconi@altimetrik.com>
2022-07-07 00:57:39 -0300

    1.2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,8 +3,33 @@ assign their values according to your own country (population in millions)
 2. Log their values to the console
  */
 
-const country = "Argentina";
-const continent = "America";
-const population = 45.38;
+// const country = "Argentina";
+// const continent = "America";
+// const population = 45.38;
 
-console.log(country, continent, population);
+// console.log(country, continent, population);
+// Coding Challenge #1
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heightJohn ** 2;
+
+// markHigherBMI = BMIMark > BMIJohn ? "yes" : "no";
+// console.log(BMIMark, BMIJohn, markHigherBMI);
